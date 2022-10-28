7b5ed38e555416ac43c499ecb0a24c7534e1ae76
nikolas-poberezhniy <nikolas.poberezhniy@gmail.com>
2022-10-28 22:16:39 +0300

    add static fetch render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {};
+
+  async componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?key=29483810-e73a753bafa1cfe0ffde3d090&q=dog&page=1&per_page=12'
+    )
+      .then(e => {
+        return e.json();
+      })
+      .then(e => {
+        this.setState({ api: [e.hits] });
+      });
+  }
+
+  componentDidUpdate() {}
+  render() {
+    const { api } = this.state;
+
+    return (
+      <div>
+        {api &&
+          api[0].map(({ id, previewURL }) => {
+            return <img key={id} src={previewURL} alt={id}></img>;
+          })}
+      </div>
+    );
+  }
+}
